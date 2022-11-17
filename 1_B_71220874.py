f52dc989fcb7158ea7d48cc7b467fc15141558f5
Fiegoldes <117885434+Fiegoldes@users.noreply.github.com>
2022-11-17 12:55:32 +0700

    Add files via upload

---
+++ 1_B_71220874.py
@@ -0,0 +1,5 @@
+print ("========== ALAT PENGHITUNG LINGKARAN SENTOT ==========")
+Diameter = int(input (" Masukkkan Diameter (Meter) : "))
+
+Hasil = 22/7 * Diameter
+print(" Jarak yang harus di tempuh : ", (round (Hasil)), "Meter")
---
+++ 2_B_71220874.py
@@ -0,0 +1,5 @@
+print ("========== INI PROGRAM SEDER HANA ==========")
+a = int(input ("Masukan Anda yang ingin di Pangkatkan : "))
+
+H = (a**7)
+print ("Angkat anda telah di pangkat kan 7! anda mendapat angka : ", H)
